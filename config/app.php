ab305c04e39fee836fd4e3364fa6d46c5a2b564f
roman jimenez <patolin00755@hotmail.com>
2017-10-17 12:31:44 -0500

    Adding Permissions

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Permissions Provider
+         */
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
